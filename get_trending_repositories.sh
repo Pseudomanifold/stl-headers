b9f5c33cabd5d30a5ab6d0098d5a96c12e4a7a01
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-03-13 17:48:13 +0100

    Updated query

--- get_trending_repositories.sh
+++ get_trending_repositories.sh
@@ -6,5 +6,4 @@ PAST_MONTH=$($DATE_CMD -d "-1 month" +%Y-%m-%d)
 curl -G https://api.github.com/search/repositories \
   --data-urlencode "sort=stars"             \
   --data-urlencode "order=desc"             \
-  --data-urlencode "q=created:>$PAST_MONTH" \
-  --data-urlencode "q=language:cpp"
+  --data "q=language:cpp+created:>$PAST_MONTH"
